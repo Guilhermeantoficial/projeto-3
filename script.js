9f08fd36f70817d08cf0c0d2a013e28dccd2100d
Guilherme Antonio Rodrigues Soares <guilhermeantoniooficial@gmail.com>
2025-02-28 15:46:14 -0300

    adição de detalhes

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 AOS.init();
+
+// Seleciona todos os itens da galeria
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+// Adiciona um evento de hover a cada item
+galleryItems.forEach(item => {
+    const imageTitle = item.querySelector('.image-title');
+
+    item.addEventListener('mouseenter', () => {
+        imageTitle.style.bottom = '0'; // Exibe o título
+    });
+
+    item.addEventListener('mouseleave', () => {
+        imageTitle.style.bottom = '-100%'; // Esconde o título
+    });
+});
